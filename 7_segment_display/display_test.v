8138bfd7a5b5202d5dce6c7ec7cee42c4578ab44
imuguruza <inigo.muguruza89@gmail.com>
2020-04-20 11:16:33 +0200

    Fisnish tb, fix pcf and display test module

--- 7_segment_display/display_test.v
+++ 7_segment_display/display_test.v
@@ -6,19 +6,23 @@
 // ================================
 
 `default_nettype none
-
-// Include clock divider constants
 `include "clk_divider_const.vh"
 
-module display_test(
-           input wire clk,                    //-- Input clock
+module display_test (
+           input wire clk,               //-- Input clock
            output reg [DW-1: 0] led_port,//-- LED matrix value (ON/OFF)
            output reg c);                //-- ON/OFF Control pin, '1' first
                                          //-- '0' second display
 
 parameter AW = 8;
 parameter DW = 7;
+parameter refresh_rate = `F_50Hz;
+
 localparam COUNT_MAX = 255;
+
+//Auxiliary wires to connect to decoder
+wire [DW-1: 0] wire_led_port;
+wire wire_c;
 
 // Disable reset
 reg reset = 0;
@@ -27,30 +31,50 @@ reg enable = 1;
 reg [AW-1:0] counter = 0;
 
 wire Hz_clock;
+//reg Hz_clock = 0;
+//-- Numer of bits to store in seg generator
+parameter M = `F_1Hz;
+localparam N = $clog2(M);
+//-- 1Hz counter register
+reg [N-1:0] divcounter = 0;
 
-// 1Hz counter
-// !! This is wrong !!
-always @(posedge clk and posedge Hz_clock)
+// Counter
+always @(posedge clk)
+  if (enable)
+    // Count
+    divcounter <= (divcounter == M - 1) ? 0 : divcounter + 1;
+  else
+    //-- Hold value
+    divcounter <= M - 1;
+
+// Generate one pulse each time divcounter = 0,
+// if it's enable, will output a '1'
+assign Hz_clock = (divcounter == 0) ? enable : 0;
+//always @(posedge clk)
+//  HZ_clock <= (divcounter == 0) ? enable : 0;
+
+// Use sync'd clk and HZ_clock to increment display value counter
+always @(posedge clk)
+  if (Hz_clock && enable)
     counter <= (counter == COUNT_MAX) ? 0 : counter + 1;
 
-
-// Instantiate clk_divider to obtain counter edges
-clk_divider #(.F(`F_1Hz))
-  Hz_clk(
-    .clk(clk_in),
-    .en(enable),
-    .clk_out(Hz_clock)
-  );
+// Assign register ports its value
+always @(posedge clk) begin
+  led_port <= wire_led_port;
+  c <= wire_c;
+  end
 
 // Instantiate decoder and connect it to the output pins
 seven_segment_decoder #(
                         .AW(AW),
                         .DW(DW),
-                        .refresh_f(`F_50Hz)
+                        .refresh_f(refresh_rate)
                       )
                    decoder(
-                      .clk(clk_in),       //-- Input clock
+                      .clk(clk),       //-- Input clock
                       .addr(counter),     //-- Data address
                       .reset(reset),      //-- Reset
-                      .led_port(led_port),//-- LED port (ON/OFF)
-                      .c(c));             //-- Display selector pin (1 left, 0 right)
+                      .led_port(wire_led_port),//-- LED port (ON/OFF)
+                      .c(wire_c));             //-- Display selector pin (1 left, 0 right)
+
+endmodule
--- 7_segment_display/display_test_tb.v
+++ 7_segment_display/display_test_tb.v
@@ -1,32 +1,37 @@
 // ======================
-//  display_test test bench
+//  Display test test bench
 // ======================
 
-//`default_nettype none
+`default_nettype none
 
+// Include clock divider constants
+`include "clk_divider_const.vh"
 
-module display_tb();
+module display_test_tb();
 
-reg clk;
-reg [6:0] led_port;
-reg c;
+reg clk = 0;
+wire [6:0] led_port;
+wire c;
 
-display_test #()
+display_test #(
+            .M(4), //Change count delay to 4 cycles instead of 1 Hz
+            .refresh_rate(2)//Change segment refresh rate to half count time
+            )
             dut(
            .clk(clk),                    //-- Input clock
            .led_port(led_port),//-- LED matrix value (ON/OFF)
            .c(c));                //-- ON/OFF Control pin, '1' first
                                          //-- '0' second display
 always
-# 1 clk_in <= ~clk_in;
+# 1 clk <= ~clk;
 
 initial begin
-//-- Store Results
-$dumpfile("display_test_tb.vcd");
-$dumpvars(0, seven_segment_decoder_tb);
+  //-- Store Results
+  $dumpfile("display_test_tb.vcd");
+  $dumpvars(0, display_test_tb);
 
-# 200 $display("END of simulation");
-$finish;
+  # 2100 $display("END of simulation");
+  $finish;
 end
 
 endmodule
