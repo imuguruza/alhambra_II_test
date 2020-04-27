527533026ef0f120f86055d96e7b4b1e80a734bf
imuguruza <inigo.muguruza89@gmail.com>
2020-04-27 12:50:37 +0200

    Rework vga_sync module

--- vga/vga_sync.v
+++ vga/vga_sync.v
@@ -1,6 +1,15 @@
-//
-// 640x480 VGA singal generator //
-// ============================ //
+/*
+   640x480 VGA singal generator
+   ============================
+
+  - Instantiates PLL for creating 25MHz clock
+  - Creates h_sync,v_sync signals
+  - Creates display enable signal and horizontal, vertical
+    pixel position in display (h,v)
+*/
+
+`default_nettype none
+
 
 module vga_sync(
       input  wire clk_in,
@@ -8,6 +17,8 @@ module vga_sync(
       output reg  h_sync,
       output reg  v_sync,
       output wire clk_sys,
+      output reg [9:0] h_count,
+      output reg [9:0] v_count,
       output reg display_en
       );
 
@@ -21,79 +32,71 @@ reg [9:0] v_counter = 0;
 
 localparam  h_pixel_total              = 800;
 localparam  h_pixel_display            = 640;
-localparam  h_pixel_front_porch_start  = 640;
 localparam  h_pixel_front_porch_amount = 16;
-localparam  h_pixel_sync_start         = 656;
 localparam  h_pixel_sync_amount        = 96;
-localparam  h_pixel_back_porch_start   = 752;
 localparam  h_pixel_back_porch_amount  = 48;
 
 localparam  v_pixel_total              = 525;
 localparam  v_pixel_display            = 480;
-localparam  v_pixel_front_porch_start  = 480;
 localparam  v_pixel_front_porch_amount = 10;
-localparam  v_pixel_sync_start         = 489;
 localparam  v_pixel_sync_amount        = 2;
-localparam  v_pixel_back_porch_start   = 492;
 localparam  v_pixel_back_porch_amount  = 33;
 
-/*
-always @( posedge sys_clk) begin
-  if (reset) begin
-    //Reset counter values
-    h_counter <= 0;
-    v_counter <= 0;
-    end
-end
-*/
+
 always @(posedge sys_clk) begin
 
   if (reset) begin
     //Reset counter values
     h_counter <= 0;
     v_counter <= 0;
+    display_en <= 0;
   end
 
-  // Generate display enable signal
-  if (h_counter < h_pixel_total && v_counter < v_pixel_total)
-    display_en <= 1;
   else
-    display_en <= 0;
+    begin
+    // Generate display enable signal
+    if (h_counter < h_pixel_display && v_counter < v_pixel_display)
+      display_en <= 1;
+    else
+      display_en <= 0;
 
     //Check if horizontal has arrived to the end
-    if (h_counter >= h_pixel_total-1)
+    if (h_counter >= h_pixel_total)
       begin
         h_counter <= 0;
         v_counter = v_counter + 1;
-      end
-   else
-    begin
-      //horizontal increment pixel value
-      h_counter <= h_counter + 1;
+        end
+    else
+        //horizontal increment pixel value
+        h_counter <= h_counter + 1;
       // check if vertical has arrived to the end
-      if (v_counter >= v_pixel_total -1)
-      v_counter <= v_counter + 1;
-      else
+      if (v_counter >= v_pixel_total)
         v_counter <= 0;
-    end
+  end
 end
 
 always @(posedge sys_clk) begin
   // Check if sync_pulse needs to be created
-  if (h_counter <= h_pixel_sync_start
-      || h_counter >= h_pixel_sync_start + h_pixel_sync_amount )
-    h_sync <= 1;
-  else
+  if (h_counter >= (h_pixel_display + h_pixel_front_porch_amount)
+      && h_counter <= (h_pixel_display + h_pixel_front_porch_amount + h_pixel_sync_amount) )
     h_sync <= 0;
+  else
+    h_sync <= 1;
 //end
 
 //always @(posedge sys_clk) begin
   // Check if sync_pulse needs to be created
-  if (v_counter <= v_pixel_sync_start
-      || v_counter >= v_pixel_sync_start + v_pixel_sync_amount )
-    h_sync <= 1;
+  if (v_counter >= (v_pixel_display + v_pixel_front_porch_amount)
+      && v_counter < (v_pixel_display + v_pixel_front_porch_amount + v_pixel_sync_amount) )
+    v_sync <= 0;
   else
-    h_sync <= 0;
+    v_sync <= 1;
+end
+
+// Route h_/v_counter to out
+always @ (posedge sys_clk) begin
+  h_count <= h_counter;
+  v_count <= v_counter;
 end
 
 assign clk_sys = sys_clk;
@@ -103,5 +106,8 @@ pll sys_clock(
 	.clock_out(sys_clk),
 	.locked(locked)
 	);
+
+//ONLY IN simulation
+//assign sys_clk = clk_in;
 
 endmodule
