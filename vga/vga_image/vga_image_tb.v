0196312ad3255f4bd1eb095dc3399a546338580a
imuguruza <inigo.muguruza89@gmail.com>
2020-04-30 17:44:25 +0200

    Add rgb port to sim

--- vga/vga_image/vga_image_tb.v
+++ vga/vga_image/vga_image_tb.v
@@ -6,7 +6,7 @@
 `timescale 1 ns/1 ns  // time-unit = 1 ns, precision = 10 ps
 
 
-module vga_sync_tb();
+module vga_image_tb();
 
 //-- Input registers
 reg clk_in = 0;
@@ -22,15 +22,18 @@ wire [9:0] h_count;
 wire [9:0] v_count;
 wire display_en;
 
-vga_sync vga_dut(
+wire [8:0] rgb_port;
+wire clk_led;
+wire locked_led;
+
+vga_image dut(
       .clk_in(clk_in),
       .reset(reset),
+      .rgb_port(rgb_port),
       .h_sync(h_sync),
       .v_sync(v_sync),
-      .clk_sys(clk_sys),
-      .h_count(h_count),
-      .v_count(v_count),
-      .display_en(display_en)
+      .clk_led(clk_led),
+      .locked_led(locked_led)
       );
 
 always
@@ -38,8 +41,8 @@ always
 
 initial begin
   //-- Store Results
-  $dumpfile("vga_sync_tb.vcd");
-  $dumpvars(0, vga_sync_tb);
+  $dumpfile("vga_image_tb.vcd");
+  $dumpvars(0, vga_image_tb);
 
   # 33728000 $display("END of simulation");
   $finish;
