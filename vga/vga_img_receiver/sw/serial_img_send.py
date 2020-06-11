0276d313de32ba1f707979fa21123653dba604db
imuguruza <inigo.muguruza89@gmail.com>
2020-06-11 16:39:02 +0200

    Keep working on script

--- vga/vga_img_receiver/sw/serial_img_send.py
+++ vga/vga_img_receiver/sw/serial_img_send.py
@@ -1,7 +1,7 @@
 import serial
 import sys
+from time import sleep
 
-print(sys.argv[1], sys.argv[2])
 if len(sys.argv) < 3 :
     print ("Usage: Serial port, image file in hex format. For example: /dev/ttyUSB0 test.mem")
 dev = sys.argv[1]
@@ -16,16 +16,37 @@ ser = serial.Serial(
 	bytesize=serial.EIGHTBITS
 )
 #ser.open()
+ser.reset_output_buffer()
 print("Serial open?", end =" ")
 print(bool(ser.is_open))
-f = open(file, "rb")
-byte = f.read(1)
+count = 0;
+
+# Problem => ff is sent 0f - 0f
+# f = open(file, "rb")
+# byte = f.read(3).replace(b'\n', b'')
+# #ser.flush()
+# while byte:
+#     ser.write(byte)
+#     print(byte)
+#     sleep(0.01)
+#     count += 1
+#     byte = f.read(3).replace(b'\n', b'')
+
+
+# Try to read as string and convert to Hex after
+# This sends just 0s
+f = open(file, "r")
+byte = f.read(3).replace('\n', '')
+#ser.flush()
 while byte:
-    #print(byte)
-    byte = f.read(1)
-    if byte != '\n' :
-        print(byte)
-        ser.write(byte)
-        ser.flush()
+    send = int(byte,16)
+    #print (send)
+    ser.write(send)
+    #print(int(byte,16))
+    sleep(0.01)
+    count += 1
+    byte = f.read(3).replace('\n', '')
+
+print (str(count) + " bytes sent...")
 print("Image sent, closing port and exiting...\n")
 ser.close()
